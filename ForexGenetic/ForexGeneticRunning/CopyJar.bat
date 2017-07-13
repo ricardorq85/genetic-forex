9d8fa4741e930e7275706830832170a9c879e755
ricardorq85 <ricardorq85@gmail.com>
2017-07-13 11:24:34 -0500

    copy ForexGenetic.jar d:\ForexGeneticRunning\Estrategia_Tester_EURJPY\

--- ForexGenetic/ForexGeneticRunning/CopyJar.bat
+++ ForexGenetic/ForexGeneticRunning/CopyJar.bat
@@ -1,3 +1,4 @@
+copy ForexGenetic.jar d:\ForexGeneticRunning\Estrategia_Tester_EURJPY\
 copy d:\ricardorq85\JavaProjects\Git\genetic-forex\ForexGenetic\ForexGeneticRunning\ForexGenetic.jar d:\ricardorq85\JavaProjects\ForexGeneticRunning\Estrategia_Tester_EURJPY\
 copy d:\ricardorq85\JavaProjects\Git\genetic-forex\ForexGenetic\ForexGeneticRunning\\ForexGenetic.jar d:\ricardorq85\JavaProjects\ForexGeneticRunning\Estrategia_Tester_USDCAD\
 pause 
