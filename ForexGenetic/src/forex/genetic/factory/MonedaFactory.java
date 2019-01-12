96e3525ee08cc922b0963210c29e876caf15ce92
Angela <ricardorq85@gmail.com>
2019-01-12 16:17:51 -0500

    Consulta de moneda usando data client

--- ForexGenetic/src/forex/genetic/factory/MonedaFactory.java
+++ ForexGenetic/src/forex/genetic/factory/MonedaFactory.java
@@ -1,49 +1,47 @@
 package forex.genetic.factory;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import forex.genetic.dao.oracle.OracleParametroDAO;
+import forex.genetic.dao.IParametroDAO;
 import forex.genetic.entities.Moneda;
 import forex.genetic.entities.MonedaEURJPY;
 import forex.genetic.entities.MonedaUSDCAD;
 import forex.genetic.exception.GeneticDAOException;
-import forex.genetic.util.jdbc.JDBCUtil;
 
 public class MonedaFactory {
 
 //	private static Connection conn;
-//	private static OracleParametroDAO parametroDAO;
+	private static IParametroDAO parametroDAO;
 	private static boolean consultado = false;
 	private static Moneda moneda;
 
-//	static {
-//		try {
+	static {
+		try {
+			parametroDAO = DriverDBFactory.createDataClients().get(0).getDaoParametro();
+		} catch (GeneticDAOException e) {
+			e.printStackTrace();
+		}
 //			conn = JDBCUtil.getConnection();
-//			parametroDAO = new OracleParametroDAO(conn);
-//		} catch (ClassNotFoundException | SQLException e) {
-//			e.printStackTrace();
-//		}
-//	}
+		// parametroDAO = new OracleParametroDAO(conn);
+	}
 
 	public static Moneda getMoneda() {
 		if (!consultado) {
 			String nombreParametro = "MONEDA";
-			String m = "USDCAD";
-//			if (parametroDAO != null) {
-//				try {
+			String m = null;
+			if (parametroDAO != null) {
+				try {
+					m = DriverDBFactory.createDataClients().get(0).getDaoParametro().getValorParametro(nombreParametro);
 //					m = parametroDAO.getValorParametro(nombreParametro);
-//				} catch (GeneticDAOException e) {
-//					m = null;
-//					e.printStackTrace();
-//				}
-//			}
+				} catch (GeneticDAOException e) {
+					m = null;
+					e.printStackTrace();
+				}
+			}
 			moneda = getMoneda(m);
 			consultado = true;
 		}
 		return moneda;
 	}
-	
+
 	public static Moneda getMoneda(String m) {
 		Moneda moneda = null;
 		if ((m == null) || ("USDCAD".equals(m))) {
@@ -51,7 +49,7 @@ public class MonedaFactory {
 		} else if ("EURJPY".equals(m)) {
 			moneda = new MonedaEURJPY();
 		}
-		return moneda;		
+		return moneda;
 	}
 
 }
